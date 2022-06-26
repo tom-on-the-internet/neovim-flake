b62ee0024a3ea8851f6bceacebcddb23d28d7a95
Thomas Steven <thomaspatricksteven@gmail.com>
2022-06-26 13:00:09 -0400

    wip

--- modules/lsp/langs/rust.nix
+++ modules/lsp/langs/rust.nix
@@ -1,37 +1,24 @@
-{
-  config,
-  lib,
-  pkgs,
-  ...
-}:
+{ config, lib, pkgs, ... }:
 with lib;
-with builtins; let
+with builtins;
+let
   cfg = config.vim.lsp.lang;
-  writeIf = cond: msg:
-    if cond
-    then msg
-    else "";
-  luaBool = b:
-    if b
-    then "true"
-    else "false";
+  writeIf = cond: msg: if cond then msg else "";
+  luaBool = b: if b then "true" else "false";
 in {
   config.vim = mkIf cfg.rust.enable {
     plenary = mkIf cfg.rust.crates.enable true;
 
     startPlugins = with pkgs.neovimPlugins; [
-      (
-        if cfg.rust.crates.enable
-        then crates
-        else null
-      )
+      (if cfg.rust.crates.enable then crates else null)
       rust-tools
     ];
 
-    completion.sources.crates.enable = mkIf (cfg.rust.crates.enable && cfg.rust.crates.completion) true;
+    completion.sources.crates.enable =
+      mkIf (cfg.rust.crates.enable && cfg.rust.crates.completion) true;
 
     lsp.afterLSP = ''
-          ${writeIf cfg.rust.crates.enable ''require('crates').setup()''}
+          ${writeIf cfg.rust.crates.enable "require('crates').setup()"}
 
           require('rust-tools').setup {
           	server = {
